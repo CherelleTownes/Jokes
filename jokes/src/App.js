f4cd1389bcee4fd34a4d69c01164b628fe2a58f1
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-03-27 15:56:55 -0400

    MVP Complete

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results:[]
+    }
+  }
+  componentDidMount = async () => {
+    const response = await axios("https://sv443.net/jokeapi/v2/joke/programming");
+    // debugger;
+    console.log(response.data);
+    this.setState({ results: response.data });
 }
 
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div> 
+        <header><h1>JOKEY JOKE</h1></header>
+
+        <h3>{results.joke}</h3>
+        <h3>{results.setup}</h3>
+        <h3>{results.delivery}</h3>
+        
+      
+      </div>
+    );
+  }
+
+}
 export default App;
